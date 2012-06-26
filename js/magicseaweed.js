7313cc57a2c81adecf0318c55b04deeae5286b82
Dan Shao <danshao@gmail.com>
2012-06-26 22:39:55 +0800

    wave chart added date and color-key

--- js/magicseaweed.js
+++ js/magicseaweed.js
@@ -8,18 +8,24 @@
 function getTT() {
 	var now = new Date();
 	var offset = now.getTimezoneOffset();
-	var imgArray = new Array();
+	var wavemap = [];
 	offset = offset / 60;
 	now.setHours(-offset);
 	now.setMinutes(0);
 	now.setSeconds(0);
 	now = Math.floor(now.getTime() / 1000);
-	var bg = "url(http://chart-a.metcentral.com/wave/750/61-" + now + "-HTSGW.png)";
-	for(var x = 0; x < 9; x++) {
-		temp = now + x*3600*3;
-		imgArray.push("http://chart-a.metcentral.com/wave/750/61-" + temp + "-HTSGW.png")
-		//temp = new Date(temp*1000);
-		//temp.setHours(offset);
+	// console.log(now);
+	for(var x = 0; x < 41; x++) {
+		image = now + x*3600*3;
+		// Convert unix timestamp back into readable date
+		date = new Date(image * 1000);
+		date = date.toGMTString();
+		wavemap.push({date: x*3 + "hr " + date, 
+			swellImage: "http://chart-a.metcentral.com/wave/750/61-" + image + "-HTSGW.png",
+			periodImage: "http://chart-a.metcentral.com/wave/750/61-" + image + "-PERPW.png",
+			windImage: "http://chart-a.metcentral.com/gfs/750/61-" + image + "-WMAG.png"
+			});
 	}
-	return imgArray;
+	console.log(wavemap);
+	return wavemap;
 }
